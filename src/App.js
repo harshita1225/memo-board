d6ae4967b8f43b4c44bbe4be6b472151e43b8d7e
harshita <harshita1225@gmail.com>
2022-11-08 21:56:55 +0100

    task completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import "./App.css";
 import { IoIosAddCircle } from "react-icons/io";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [alltodo, setAllTodo] = useState([]);
   const [edit, setEdit] = useState(false);
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      setAllTodo(items);
+    }
+  }, []);
+
+  console.log("this is item", items);
   const handleClick = () => {
-    setAllTodo([...alltodo, todo]);
-    setTodo("");
+    if (todo) {
+      setAllTodo([...alltodo, todo]);
+      setTodo("");
+      localStorage.setItem("items", JSON.stringify(alltodo));
+    } else alert("Please type a task");
   };
-  const handleEdit = () => {};
-  console.log(alltodo);
 
   return (
-    <div className="flex gap-2 h-screen justify-center items-center">
-      <div>
-        <h1>My Memo List</h1>
-        <h3>My plan for the day!!</h3>
-        <div className="bg-red-400 h-[10rem]">
+    <div className="flex  min-w-[30rem] h-screen justify-center items-center bg-[url(https://i.pinimg.com/736x/e6/73/d5/e673d5128169c3efca2a0a8db75a456c.jpg)] bg-no-repeat  bg-top  overflow-scroll">
+      <div className="min-w-[20%] flex flex-col gap-5 max-h-full">
+        <h1 className="text-indigo-900 text-bold text-[2rem] mb-5 text-center">
+          My Memo List
+        </h1>
+        <h3 className="text-bold text-[1rem] mt-2 text-center text-teal-700">
+          Lets Plan for the day!!
+        </h3>
+        <div className="flex flex-col text-[1rem] h-[22rem] w-[22rem] overflow-auto gap-5 px-3">
           {alltodo.map((item, idx) => (
             <Card
               key={idx}
@@ -32,16 +46,14 @@ function App() {
             />
           ))}
         </div>
-        <div className="flex gap-2 text-[1.5rem] items-center">
+        <div className="flex gap-2 text-[1rem] items-center mx-auto">
+          <IoIosAddCircle className="text-[2.5rem]" onClick={handleClick} />
           <input
             placeholder="Add todo to Memo List..."
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
-            className="border-slate-400 border-2 p-1"
+            className="border-slate-400 border-2 p-1 "
           />
-          <IoIosAddCircle className="text-[2.5rem]" onClick={handleClick} />
-
-          <IoIosAddCircle className="text-[2.5rem]" onClick={handleEdit} />
         </div>
       </div>
     </div>
--- src/Card.js
+++ src/Card.js
@@ -1,49 +1,77 @@
-import React, { useState } from "react";
-import { BiMessageRoundedEdit } from "react-icons/bi";
-
+import React, { useState, useEffect } from "react";
+import { FcEditImage } from "react-icons/fc";
+import { FcDeleteDatabase } from "react-icons/fc";
+import { FaHourglassHalf } from "react-icons/fa";
+import { MdFileDownloadDone } from "react-icons/md";
 const Card = ({ item, idx, alltodo, setTodo, setAllTodo }) => {
   const [status, setStatus] = useState(false);
   const [input, setInput] = useState(item);
 
   const [edit, setEdit] = useState(false);
 
-  const handleEdit = () => {
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(alltodo));
+  }, [alltodo]);
+
+  const handleEdit = (idx) => {
     setEdit(!edit);
+    //const findIdx = alltodo.findIndex((el) => el === input);
+    /*  console.log(findIdx);
+    console.log((alltodo[findIdx] = input)); */
+    //setAllTodo([...alltodo, alltodo.splice(findIdx, 1, input)]);
+
+    const oldTodos = [...alltodo];
+    oldTodos[idx] = input;
+    setAllTodo(oldTodos);
+    console.log(oldTodos);
   };
 
   const handleClick = () => {
     setAllTodo(alltodo.filter((el) => el !== item));
   };
   return (
-    <div className="flex justify-between p-2">
-      {edit ? (
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-      ) : (
-        <p>{input}</p>
-      )}
-      <div className="flex gap-2 ">
+    <div>
+      <div className="flex items-center justify-between gap-5 max-h-[5rem]">
         {edit ? (
-          <button className="border-2 p-1" onClick={handleEdit}>
-            Save
-          </button>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
         ) : (
-          <button className="border-2 p-1" onClick={handleEdit}>
-            Edit
-          </button>
+          <p>{input}</p>
         )}
-        <button
-          className={`border-2 p-1 ${status ? `bg-green-500` : `bg-red-500`}`}
-          onClick={() => setStatus(!status)}
-        >
-          {status ? "Completed" : "Pending"}
-        </button>
-        <button className="border-2 p-1" onClick={handleClick}>
-          Delete
-        </button>
+        <div className="flex gap-2">
+          {edit ? (
+            <button
+              className="border-2 p-1 rounded-md bg-slate-400"
+              onClick={() => handleEdit(idx)}
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              className="border-2 bg-slate-400 rounded-md p-1"
+              onClick={handleEdit}
+            >
+              <FcEditImage />
+            </button>
+          )}
+          <button
+            className={`border-2 p-1 rounded-md ${
+              status ? `bg-green-500` : `bg-pink-300`
+            }`}
+            onClick={() => setStatus(!status)}
+          >
+            {status ? <MdFileDownloadDone /> : <FaHourglassHalf />}
+          </button>
+          <button
+            className="border-2 p-1 bg-slate-400 rounded-md"
+            onClick={handleClick}
+          >
+            <FcDeleteDatabase />
+          </button>
+        </div>
       </div>
     </div>
   );
